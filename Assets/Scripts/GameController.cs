636bdc3727a171731dbc9ecd58d5f15e3320ceaa
vansteki <vansteki.tw@gmail.com>
2015-05-08 22:31:39 +0800

    113. simple for loop to generate hazards

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,7 @@ public class GameController : MonoBehaviour {
 	//@try serialize here to group 2 public properties in the future?
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public float hazardCount;
 	//@public Vector3 test = new Vector3(1,2,3);
 
 	void Start () {
@@ -12,11 +13,13 @@ public class GameController : MonoBehaviour {
 	}
 
 	void SpawnWaves () {
-		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (hazard, spawnPosition, spawnRotation);
+		for (int i = 0; i < hazardCount; i++) {
+			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (hazard, spawnPosition, spawnRotation);
+		}
 		//Instantiate (hazard, spawnPosition, transform.rotation);
-		Debug.Log (transform.rotation);
-		Debug.Log (spawnRotation);
+//		Debug.Log (transform.rotation);
+//		Debug.Log (spawnRotation);
 	}
 }
