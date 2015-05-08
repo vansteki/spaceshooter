21da5b323cc0c8026798b3e2b0fc4908dba93ee9
vansteki <vansteki.tw@gmail.com>
2015-05-08 13:33:53 +0800

    spawn a single astroid
    spawn a single astroid x axes astroid out side of boundary, and fly
    tword player.
    
    is it necessary to use Quaternion to define rotatoin?

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	//@try serialize here to group 2 public properties in the future?
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	//@public Vector3 test = new Vector3(1,2,3);
+
+	void Start () {
+		SpawnWaves();
+	}
+
+	void SpawnWaves () {
+		Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+		//Instantiate (hazard, spawnPosition, transform.rotation);
+		Debug.Log (transform.rotation);
+		Debug.Log (spawnRotation);
+	}
+}
