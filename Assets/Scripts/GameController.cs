9e28e482ecfe945f6ac920ab352c05fd31b1826d
vansteki <vansteki.tw@gmail.com>
2015-05-08 22:52:07 +0800

    113 C sharp's Coroutines
    wow

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,16 +7,21 @@ public class GameController : MonoBehaviour {
 	public Vector3 spawnValues;
 	public float hazardCount;
 	//@public Vector3 test = new Vector3(1,2,3);
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
 
 	void Start () {
-		SpawnWaves();
+		StartCoroutine(SpawnWaves());
 	}
 
-	void SpawnWaves () {
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds(startWait);
 		for (int i = 0; i < hazardCount; i++) {
 			Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
 			Quaternion spawnRotation = Quaternion.identity;
 			Instantiate (hazard, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds(spawnWait);
 		}
 		//Instantiate (hazard, spawnPosition, transform.rotation);
 //		Debug.Log (transform.rotation);
